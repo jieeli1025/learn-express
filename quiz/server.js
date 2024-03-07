d6500921614c4947190807093f9112f11425f188
jie <jieeli1025@gmail.com>
2024-03-06 20:51:49 -0500

    02292024

--- quiz/server.js
+++ quiz/server.js
@@ -6,35 +6,43 @@ var cors = require('cors');
 const port = 8000;
 
 let users;
-fs.readFile(path.resolve(__dirname, '../data/users.json'), function(err, data) {
+fs.readFile(path.resolve(__dirname, '../data/users.json'), function (err, data) {
   console.log('reading file ... ');
-  if(err) throw err;
+  if (err) throw err;
   users = JSON.parse(data);
 })
 
+
 const addMsgToRequest = function (req, res, next) {
-  if(users) {
+  if (users) {
     req.users = users;
     next();
   }
   else {
     return res.json({
-        error: {message: 'users not found', status: 404}
+      error: { message: 'users not found', status: 404 }
     });
   }
-  
+
 }
 
 app.use(
-  cors({origin: 'http://localhost:3000'})
+  cors({ origin: 'http://localhost:3000' })
 );
 app.use('/read/usernames', addMsgToRequest);
 
-app.get('/read/usernames', (req, res) => {
-  let usernames = req.users.map(function(user) {
-    return {id: user.id, username: user.username};
-  });
-  res.send(usernames);
+
+app.get('/read/usernames:name', (req, res) => {
+  let name = req.params.name;
+  let username = req.users.filter((user) => {
+    return user.username === name;
+
+  })
+  if (username.length === 0) {
+    res.send('No user found');
+  } else {
+    res.send(username);
+  }
 });
 
 app.use(express.json());
@@ -50,6 +58,21 @@ app.post('/write/adduser', (req, res) => {
   });
   res.send('done');
 })
+
+
+app.get('/read/username/:name', (req, res) => {
+  const { name } = req.params; // Extract the username from the URL parameter
+
+  // Find the user with the specified username
+  const user = req.users.find(user => user.username === name);
+
+  if (user) {
+    res.json(user); // Send the user object as JSON response
+  } else {
+    res.status(404).json({ error: { message: 'User not found', status: 404 } });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
